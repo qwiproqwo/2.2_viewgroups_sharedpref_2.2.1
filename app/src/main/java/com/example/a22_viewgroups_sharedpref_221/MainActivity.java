625782503cd2bcdece561c1b3a593e8d1e98dc8a
Viacheslav <qwiproqwo@mail.ru>
2019-11-04 14:36:29 +0300

    Initial commit

---
+++ app/src/main/java/com/example/a22_viewgroups_sharedpref_221/MainActivity.java
@@ -0,0 +1,46 @@
+package com.example.a22_viewgroups_sharedpref_221;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText mInputNote;
+    private Button mBtnSaveNote;
+    private SharedPreferences myNoteSharedPref;
+    private static String NOTE_TEXT = "note_text";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        initViews();
+        getDataFromSharedPref();
+    }
+
+    private void initViews() {
+        mInputNote = findViewById(R.id.inputNote);
+        mBtnSaveNote = findViewById(R.id.btnSaveNote);
+        myNoteSharedPref = getSharedPreferences(getString(R.string.MyNote), MODE_PRIVATE);
+
+        mBtnSaveNote.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                SharedPreferences.Editor myEditor = myNoteSharedPref.edit();
+                String noteTxt = mInputNote.getText().toString();
+                myEditor.putString(NOTE_TEXT, noteTxt);
+                myEditor.apply();
+                Toast.makeText(MainActivity.this, getString(R.string.data_saved), Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+    private void getDataFromSharedPref(){
+        String noteTxt = myNoteSharedPref.getString(NOTE_TEXT, "");
+        mInputNote.setText(noteTxt);
+    }
+}
